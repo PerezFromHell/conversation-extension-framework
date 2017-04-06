ad2a12d317b13421004f35aadef14cfc1a99bc45
Paul Thoresen <pthoresen@us.ibm.com>
2017-04-06 15:32:07 -0500

    20170406_1531 - Removed License Headers
    Replaced license headers with single license file for better
    readability.

--- src/process-handler/process.js
+++ src/process-handler/process.js
@@ -1,18 +1,3 @@
-/**
-* @Date:   2017-03-19T20:00:40-05:00
- * @Last modified time: 2017-04-05T23:56:29-05:00
-* @License: Licensed under the Apache License, Version 2.0 (the "License");  you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and
-  limitations under the License.
-
-* @Copyright: Copyright 2016 IBM Corp. All Rights Reserved.
-*/
-
 let processUtils = require('../utils/process')
 let conversationUtils = require('../utils/conversation')
 let apiCallDirector = require('./api-call-director')
